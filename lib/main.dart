993d84d08ab48271a54d0cee38bcde4f426e2e2d
Sumit <bharangarsinghsumit@gmail.com>
2026-01-12 22:27:10 +0530

    provider setup implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:medi_tick/screens/home_screen.dart';
 
-void main() {
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:medi_tick/models/medicine.dart';
+import 'package:medi_tick/providers/medicine_provider.dart';
+import 'package:medi_tick/screens/home_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(MedicineAdapter());
+
+  await Hive.openBox<Medicine>('medicines');
+
   runApp(const MyApp());
 }
 
@@ -10,12 +23,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => MedicineProvider())],
+
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Colors.teal,
+          primarySwatch: Colors.teal,
+          scaffoldBackgroundColor: Colors.grey[50],
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.teal,
+            foregroundColor: Colors.white,
+            elevation: 0,
+          ),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: Colors.orange, // Constraint Met
+            foregroundColor: Colors.white,
+          ),
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.teal)
+              .copyWith(
+                secondary: Colors.orange, // For accents
+              ),
+        ),
+        home: const HomeScreen(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const HomeScreen(),
     );
   }
 }
---
+++ lib/providers/medicine_provider.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:medi_tick/models/medicine.dart';
+
+class MedicineProvider with ChangeNotifier {
+  
+  List<Medicine> _medicines = [];
+
+  
+  List<Medicine> get medicines => _medicines;
+
+  
+  final String _boxName = 'medicines';
+
+  
+  void getMedicines() async {
+    var box = await Hive.openBox<Medicine>(_boxName);
+    
+    _medicines = box.values.toList();
+    
+    
+    _medicines.sort((a, b) {
+      
+      return a.scheduledTime.compareTo(b.scheduledTime);
+    });
+
+    notifyListeners();
+  }
+
+ 
+  Future<void> addMedicine({
+    required String name,
+    required int dosage,
+    required String dosageType,
+    required DateTime scheduledTime,
+  }) async {
+    var box = await Hive.openBox<Medicine>(_boxName);
+
+    
+    int notificationId = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+
+    final newMedicine = Medicine(
+      name: name,
+      dosage: dosage,
+      dosageType: dosageType,
+      scheduledTime: scheduledTime,
+      notificationId: notificationId,
+    );
+
+    
+    await box.add(newMedicine);
+    
+    // Refresh the list so the UI updates immediately
+    getMedicines(); 
+
+    
+  }
+
+  // 3. Delete Medicine
+  Future<void> deleteMedicine(int index) async {
+    var box = await Hive.openBox<Medicine>(_boxName);
+    
+    
+    await _medicines[index].delete();
+
+    getMedicines(); // Refresh list
+
+    // TODO: Cancel the specific Alarm/Notification here later
+  }
+}
